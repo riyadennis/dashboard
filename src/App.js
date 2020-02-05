507c21707f8bfc9483ecd973644e3c5640179041
Riya Dennis <riyacv@gmail.com>
2020-02-05 12:20:13 +0000

    initial learning

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 style={{color: "red"}}> Riya Dennis</h1>
+                <p>
+                    Software engineer in London from India, Kerala.
+                </p>
+                <ol>
+                    <li>Sardinia</li>
+                    <li>Egypt</li>
+                    <li>Morocco</li>
+                </ol>
+            </div>
+        )
+    }
+}
+export default App
