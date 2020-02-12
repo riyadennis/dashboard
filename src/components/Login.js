80a18550d58a058358ec0168d5ff145334ff296f
Riya Dennis <riyacv@gmail.com>
2020-02-12 14:10:59 +0000

    moved axios calls to seperate function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import LoginForm from "./LoginContainer"
-import axios from "axios";
+import ApiLogin from "./ApiLogin";
 
 class Login extends React.Component{
     constructor(props) {
@@ -20,33 +20,8 @@ class Login extends React.Component{
         })
     }
     handleSubmit(event){
-        console.log('form submitted: ' + this.state.email);
-        let loginCall = function (data, props, callback) {
-            axios.post('http://localhost:8081/login/',
-                {
-                    email: data.email,
-                    password: data.password
-                },{
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                }
-            )
-                .then(function (response) {
-                    if (response.status === 200){
-                        props.handler()
-                        callback(props, response.data)
-                    }
-                })
-                .catch(function (error) {
-                    HandleAxiosError(error)
-                    console.log(error.config);
-                });
-        }
-        loginCall(this.state, this.props, function(props, response) {
-            props.handler()
-            console.log(response)
-        });
+        // call login endpoint
+        ApiLogin(this.state, this.props);
         event.preventDefault();
     }
 
@@ -57,21 +32,4 @@ class Login extends React.Component{
     }
 }
 
-function HandleAxiosError(error){
-    if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("response data "+error.response.data);
-        console.log("response status "+error.response.status);
-        console.log("response headers "+error.response.headers);
-    } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log("request error"+error.request);
-    } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-    }
-}
 export default Login
