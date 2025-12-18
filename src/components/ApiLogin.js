199f0e4a7fa9b08a08957ce143ea6489f58276b2
Riya Dennis <riyacv@gmail.com>
2025-12-18 15:10:15 +0000

    feat: removed rest end point and used graphql query for login

--- src/components/ApiLogin.js
+++ src/components/ApiLogin.js
@@ -1,41 +1,34 @@
-import axios from "axios";
+import { useState } from 'react';
+import { gql } from '@apollo/client';
+import { useMutation } from '@apollo/client/react';
+
+const LOGIN = gql`
+ mutation Login($input: LoginInput!){
+	Login(input: $input){
+		status
+		accessToken
+		expiry
+		tokenType
+		lastRefresh
+		tokenTTL
+	}
+}
+`;
 
 function ApiLogin(state, props){
-    axios.post('http://localhost:8011/login/',
-        {
-            email: state.email,
-            password: state.password
-        },{
-            headers: {
-                'Content-Type': 'application/json',
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [login] = useMutation(LOGIN, {
+        variables: {
+            input: {
+                email: email,
+                password: password
             }
-        }
-    )
-        .then(function (response) {
-            props.handler(response.data)
-        })
-        .catch(function (error) {
-            HandleError(error)
-            console.log(error.config);
-        });
+          }
+      });
+    login();
+    props.handler(login.data.Login)
 }
 
-function HandleError(error){
-    if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("response data "+error.response.data);
-        console.log("response status "+error.response.status);
-        console.log("response headers "+error.response.headers);
-    } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log("request error"+error.request);
-    } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-    }
-}
 
 export default ApiLogin
